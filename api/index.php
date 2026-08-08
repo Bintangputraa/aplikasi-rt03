e73f84a1080800b277239a68ac7e9da74fedba5d
Bintangputraa <bintang.cahya.putraa@gmail.com>
2026-08-09 04:23:28 +0700

    Update /api/index.php

--- api/index.php
+++ api/index.php
@@ -1,3 +1,16 @@
 <?php
-// Meneruskan request dari Vercel ke public/index.php Laravel
+
+// Membuat folder penyimpanan sementara di lingkungan serverless Vercel
+$storageDirs = [
+    '/tmp/framework/views',
+    '/tmp/framework/sessions',
+    '/tmp/framework/cache',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
